9f26ba864c03415b4a338b0ff0fc34888055414a
David Senk <david@senk.me>
2023-12-27 18:56:51 -0500

    getlist helper

---
+++ tsync/getlists.sh
@@ -0,0 +1,1 @@
+curl --request GET --url "https://api.trello.com/1/boards/5e39f09e6e6cd97a9702a23e/lists/all?key=$(jq -r '.settings .trelloApiKey'  < appsettings.json)&token=$(jq -r ".settings .trelloUserToken" < appsettings.json)" --header 'Accept: application/json' | jq '.[] | {name, id, closed}'
