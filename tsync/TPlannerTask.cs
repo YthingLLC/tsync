19136f1c197900244ab970a9fe43ab5f8544f573
David Senk <david@senk.me>
2024-01-18 22:51:05 -0500

    applied apache license

--- tsync/TPlannerTask.cs
+++ tsync/TPlannerTask.cs
@@ -1,3 +1,20 @@
+/*
+Copyright 2024 Ything LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
 using System.Text.Json.Serialization;
 
 namespace tsync;
@@ -66,6 +83,9 @@ public struct TPlannerTaskExternalReference
 
     public String type => "Other";
     
+    //Graph requests fail if this is provided on a PATCH request
+    //so... just toss it for now?
+    [JsonIgnore]
     public DateTimeOffset? lastModifiedDateTime { get; init; }
     
     //no, I am not going to support lastModifiedBy. I don't care.
